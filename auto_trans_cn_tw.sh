c6902f63436c4d13710db688778990b3ad69d2ba
Shuai YUAN <yszheda@gmail.com>
2013-04-15 01:32:59 +0800

    update tw version

--- auto_trans_cn_tw.sh
+++ auto_trans_cn_tw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm tw/index.html
-opencc -i cn/index.html -o tw/index.html -c zhs2zhtw_p.ini
+opencc -i cn/index.html -o tw/index.html -c zhs2zhtw_vp.ini
 # opencc -i tw/index.html -o cn/index.html -c zhtw2zhcn_s.ini
